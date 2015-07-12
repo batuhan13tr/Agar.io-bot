f2178b47a0e6f49cc780d4ac4abf818526bec647
batuhan13tr <batuhan_06_21@hotmail.com>
2015-07-12 20:12:59 +0300

    Update beta.user.js

--- beta.user.js
+++ beta.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        AposBotBeta
 // @namespace   AposBotBeta
-// @include     http://agar.io/*
+// @include     agaryator.com/*
 // @version     3.46
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
